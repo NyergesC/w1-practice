f116cbdeba87d419c8ef22d919cb4e90439a1093
NyergesC <cintia.nyerges@gmail.com>
2022-01-20 13:30:34 +0100

    getting familiar data

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,47 @@
+/*  const data = [
+    "Gergely Kiss",
+    "Krisztian Bui",
+    "Buczko Balazs",
+    "Andras Varga",
+    "Borbala Terez"
+];
+ */ 
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favoritefood: "csirke"
+    },
+    {
+        name: "Krisztian Bui",
+        favoritefood: "banan"
+    },
+    {
+        name: "Buczko Balazs",
+        favoritefood: "rajas pizza"
+    },
+    {
+        name: "Andras Varga",
+        favoritefood: "brassoi"
+    },
+    {
+        name: "Borbala Terez",
+        favoritefood: "avokado"
+     },
+
+]
 function loadEvent() {
     console.log ("az oldal betoltodott")
 
-    const list = document.querySelectorAll("section")
+    const root = document.querySelector("#root")
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
-        ` )
-        }
+    for (const frontendstudent of data) {
+        root.insertAdjacentHTML("beforeend",`
+            <section>
+                <h1>${frontendstudent.name}</h1>
+                <p>${frontendstudent.favoritefood}</p>
+            </section>
+        `)
+    }
 }
 window.addEventListener("load", loadEvent) 
